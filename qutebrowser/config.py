331a3bd9c498cc8a298b3a191982b2a2fb03a071
luan441 <luanhenrique441@gmail.com>
2024-02-09 19:43:56 -0300

    update default engines qutebrowser

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -4,6 +4,11 @@ config.load_autoconfig()
 
 c.url.start_pages = ["https://google.com.br"]
 
+c.url.searchengines = {
+    'DEFAULT': 'https://google.com.br/search?q={}',
+    'yt': 'https://youtube.com/results?search_query={}'
+}
+
 c.fonts.default_family = "FiraCode Nerd Font"
 
 dracula.draw.blood(c, {
