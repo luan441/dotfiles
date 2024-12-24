bdce2d21f2abcbb4e8911b2d8ed7f554b74cc95a
luan441 <vloid@proton.me>
2024-12-23 23:29:07 -0300

    feat(nvim): telescope search hidden files

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,13 +1,19 @@
 return {
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
   },
   {
     'nvim-telescope/telescope-ui-select.nvim',
     config = function()
-      -- This is your opts table
       require("telescope").setup {
+        pickers = {
+          find_files = {
+            -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+            hidden = true
+          },
+        },
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown()
